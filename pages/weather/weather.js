f6518da231ee08f1553f1c28df5d2ae2c35a5028
walden <weidf@163.com>
2016-11-07 23:56:00 +0800

    添加从百度天气请求网络数据, 并在列表中加载出来.

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -2,13 +2,60 @@
 var util = require('../../utils/util.js')
 Page({
   data: {
-    logs: []
+    city: "-",
+    fengli: '-',
+    forecast: []
   },
+  
+  
+  
+  
+  
+  
+    weather: {
+      request: function () {
+        
+      },
+      analysisData: function (data) {
+        console.log("analysisData")
+      }
+    },
+  
+  
+  
+  
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
+
+    var that = this
+
+  wx.request({
+          url: 'https://apis.baidu.com/apistore/weatherservice/recentweathers',
+          data:{
+            cityid: '101020100'
+          },
+          header:{ 
+            'content-type': 'application/json',
+            'apikey': 'b7361685b2ad57891845773c8fc56458'
+          },
+          method:"GET",
+          success: function (res) {
+            var weatherInfo = res.data.retData;
+            that.setData({
+              city: weatherInfo.city,
+              fengli: weatherInfo.today.fengli,
+              forecast: weatherInfo.forecast,
+              intro: weatherInfo.today.index,
+            })
+          },
+          fail: function (err) {
+            console.log(JSON.stringify(err))
+          }
+        })
+
+
+  },
+  
+
+
+
 })
